79d67f166bed45d89f41f430db9ff6ac4c73a2bb
Aaron Osorio <aaronosorio2000@gmail.com>
2022-12-19 14:09:00 -0600

    Erorres

--- src/components/publico/register/register.js
+++ src/components/publico/register/register.js
@@ -1,21 +1,21 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { ContextAuth } from "../../../Memory/Auth";
-import { register } from "../../../services/Auth";
-import Credentials from "../../shared/Credentials";
+// import { useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { ContextAuth } from "../../../Memory/Auth";
+// import { register } from "../../../services/Auth";
+// import Credentials from "../../shared/Credentials";
 
-function Register() {
-  const navigate = useNavigate();
-  const [auth, sendAuth] = useContext(ContextAuth);
-  const send = async (form) => {
-    const token = await register(form);
-    sendAuth({ tipo: "colocar", token });
-    navigate("/list");
-  };
+// function Register() {
+//   const navigate = useNavigate();
+//   const [auth, sendAuth] = useContext(ContextAuth);
+//   const send = async (form) => {
+//     const token = await register(form);
+//     sendAuth({ tipo: "colocar", token });
+//     navigate("/list");
+//   };
 
-  return (
-    <Credentials send={send} title="Register" button="Signup"></Credentials>
-  );
-}
+//   return (
+//     <Credentials send={send} title="Register" button="Signup"></Credentials>
+//   );
+// }
 
-export default Register;
+// export default Register;
