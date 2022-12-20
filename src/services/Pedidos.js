0afb6d069f5bb841215411107d42e7f862271800
Aaron Osorio <aaronosorio2000@gmail.com>
2022-12-20 04:19:24 -0600

    conexion api final

--- src/services/Pedidos.js
+++ src/services/Pedidos.js
@@ -1,44 +1,57 @@
 export async function pedirGoals() {
-  const response = await fetch("/api/goals");
+  const response = await fetch(
+    "https://api-production-0143.up.railway.app/api/api/goals"
+  );
   const goals = await response.json();
   return goals;
 }
 
 export async function pedirGoal(id) {
-  const response = await fetch(`/api/goals${id}`);
+  const response = await fetch(
+    `https://api-production-0143.up.railway.app/api/api/goals${id}`
+  );
   const goal = await response.json();
   return goal;
 }
 
 export async function createGoal(goal) {
-  const response = await fetch("/api/goals", {
-    method: "POST",
-    body: JSON.stringify(goal),
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-    },
-  });
+  const response = await fetch(
+    "https://api-production-0143.up.railway.app/api/api/goals",
+    {
+      method: "POST",
+      body: JSON.stringify(goal),
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
   const createdGoal = await response.json();
   console.log("Meta creada", createdGoal);
   return createdGoal;
 }
 
 export async function updateGoal(goal) {
-  const response = await fetch(`/api/goals/${goal.id}`, {
-    method: "PUT",
-    body: JSON.stringify(goal),
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-    },
-  });
+  const response = await fetch(
+    `https://api-production-0143.up.railway.app/api/goals/${goal.id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify(goal),
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
   const updatedGoal = await response.json();
   console.log("Meta actualizada", updatedGoal);
   return updatedGoal;
 }
 
 export async function borrarGoal(id) {
-  await fetch(`/api/goals/${id}`, {
-    method: "DELETE",
-  });
+  await fetch(
+    `https://api-production-0143.up.railway.app/api/api/goals/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
   console.log("Meta borrada", id);
 }
