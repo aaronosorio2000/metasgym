c4282213563f4d33940dfcde1257d44acdbce8b6
Aaron Osorio <aaronosorio2000@gmail.com>
2022-12-20 04:46:30 -0600

    correcion conexion api

--- src/services/Pedidos.js
+++ src/services/Pedidos.js
@@ -1,53 +1,43 @@
 export async function pedirGoals() {
-  const response = await fetch(
-    "https://api-production-0143.up.railway.app/api/goals"
-  );
+  const response = await fetch("/api/goals");
   const goals = await response.json();
   return goals;
 }
 
 export async function pedirGoal(id) {
-  const response = await fetch(
-    `https://api-production-0143.up.railway.app/api/goals${id}`
-  );
+  const response = await fetch(`/api/goals${id}`);
   const goal = await response.json();
   return goal;
 }
 
 export async function createGoal(goal) {
-  const response = await fetch(
-    "https://api-production-0143.up.railway.app/api/goals",
-    {
-      method: "POST",
-      body: JSON.stringify(goal),
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    }
-  );
+  const response = await fetch("/api/goals", {
+    method: "POST",
+    body: JSON.stringify(goal),
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  });
   const createdGoal = await response.json();
   console.log("Meta creada", createdGoal);
   return createdGoal;
 }
 
 export async function updateGoal(goal) {
-  const response = await fetch(
-    `https://api-production-0143.up.railway.app/api/goals/${goal.id}`,
-    {
-      method: "PUT",
-      body: JSON.stringify(goal),
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    }
-  );
+  const response = await fetch(`/api/goals/${goal.id}`, {
+    method: "PUT",
+    body: JSON.stringify(goal),
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  });
   const updatedGoal = await response.json();
   console.log("Meta actualizada", updatedGoal);
   return updatedGoal;
 }
 
 export async function borrarGoal(id) {
-  await fetch(`https://api-production-0143.up.railway.app/api/goals/${id}`, {
+  await fetch(`/api/goals/${id}`, {
     method: "DELETE",
   });
   console.log("Meta borrada", id);
